7fdb1b44a93e47490cd196c0e991cbc9e4a30270
Vincent Sellier <vincent.sellier@gmail.com>
2020-01-25 15:05:37 +0100

    variabilize image version

--- build-site.sh
+++ build-site.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+IMAGE=jekyll/jekyll:stable
+
 echo "**** Checking if there is a more recent jekyll image..."
-docker pull jekyll/jekyll:stable
+docker pull ${IMAGE}
 
 echo "**** Building the site..."
-docker run -ti -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -e JEKYLL_ENV=production -p 4000:4000 --rm --name jekyll jekyll/jekyll:stable jekyll build ./
-
+docker run -ti -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -e JEKYLL_ENV=production -p 4000:4000 --rm --name jekyll ${IMAGE} jekyll build ./
--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker run -ti -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -p 4000:4000 --rm --name jekyll jekyll/jekyll:stable jekyll serve --draft ./
+IMAGE=jekyll/jekyll:stable
+
+docker pull ${IMAGE}
+docker run -ti -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -p 4000:4000 --rm --name jekyll ${IMAGE} jekyll serve --draft ./
 
