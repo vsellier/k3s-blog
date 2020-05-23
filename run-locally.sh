ebfd1593784d6acc1eeec9024ef274edf265e8c0
Vincent Sellier <vincent.sellier@gmail.com>
2020-05-24 00:00:17 +0200

    fix issue to start locally with last jekyll version

--- run-locally.sh
+++ run-locally.sh
@@ -3,5 +3,6 @@
 IMAGE=jekyll/jekyll:stable
 
 docker pull ${IMAGE}
-docker run -ti -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -p 4000:4000 --rm --name jekyll ${IMAGE} jekyll serve --draft ./
+
+docker run -ti --rm -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -v "$(pwd)/run.sh:/run.sh" -p 4000:4000 --name jekyll --entrypoint /run.sh ${IMAGE}
 
