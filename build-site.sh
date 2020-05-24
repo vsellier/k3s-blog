7848e0010e33d19edd1f6fbd88eefa977220db18
Vincent Sellier <vincent.sellier@gmail.com>
2020-05-24 12:53:20 +0200

    fix jekyll run without local gems

--- build-site.sh
+++ build-site.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-IMAGE=jekyll/jekyll:stable
+IMAGE=jekyll/builder:stable
 
 echo "**** Checking if there is a more recent jekyll image..."
 docker pull ${IMAGE}
 
 echo "**** Building the site..."
-docker run -ti -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -e JEKYLL_ENV=production -p 4000:4000 --rm --name jekyll ${IMAGE} jekyll build ./
+docker volume create gem
+docker run -ti  --rm -v gems:/usr/gem -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -e JEKYLL_ENV=production -p 4000:4000 --name jekyll ${IMAGE} jekyll build ./
--- run-locally.sh
+++ run-locally.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 
 IMAGE=jekyll/jekyll:stable
 
 docker pull ${IMAGE}
 
-docker run -ti --rm -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -v "$(pwd)/run.sh:/run.sh" -p 4000:4000 --name jekyll --entrypoint /run.sh ${IMAGE}
+docker volume create gem
 
+docker run -ti --rm -v gem:/usr/gem -v "${PWD}/blog:/src" -w /src -v "${PWD}/build_cache/bundle:/usr/local/bundle" -v "${PWD}/build_cache/jekyll:/home/jekyll/.bundle" -v "$(pwd)/run.sh:/run.sh" -p 4000:4000 --name jekyll --entrypoint /run.sh ${IMAGE}
